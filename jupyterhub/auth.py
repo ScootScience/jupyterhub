a3d398b7af563ef603fba0936c4400a68f39b761
Connor Dibble <cddibble@gmail.com>
2021-06-30 11:16:18 -0700

    using handler.proxy object to delete user when auth is expired

--- jupyterhub/auth.py
+++ jupyterhub/auth.py
@@ -564,7 +564,11 @@ class Authenticator(LoggingConfigurable):
                 # await handler.stop_single_user(user, user.spawner.name)
                 print(user.server)
                 # await self.proxy.delete_user(user, server_name='')
-                await self.delete_user(user, server_name='')
+                for server_name in user.spawners:
+                    print('sever_name from user.spawners:')
+                    print(server_name)
+                await handler.proxy.delete_user(user, server_name = '')
+                # await self.delete_user(user, server_name='')
                 handler.clear_login_cookie()
                 handler.clear_cookie("jupyterhub-hub-login")
                 handler.clear_cookie("jupyterhub-session-id")
